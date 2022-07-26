1d0eea4175d79ccd1ebb782b30895556e48bfa2f
Qwe3301 <99192815+Qwe3301@users.noreply.github.com>
2022-07-25 23:39:44 -0300

    arrumei mais o codigo e coloquei uma funcao de porcentagem

--- javascript.js
+++ javascript.js
@@ -1,14 +1,45 @@
 import { annotate, annotationGroup } from "https://unpkg.com/rough-notation?module"
 
+const texto = document.getElementById("texto")
 const body = document.getElementById("texto_digitado")
 const div_erro = document.getElementById('div_erros')
-const texto = document.getElementById("texto")
 
-const botao = document.getElementById("botao").addEventListener("click", input_txt)
+
+const sublinhado = document.getElementsByClassName("sublinhado")[0]
+const quadro = document.getElementsByClassName("quadro")[0]
+const marcador = document.getElementsByClassName("marcador")[0]
+
+const tipo_underline = "underline"
+const tipo_box = "box"
+const tipo_highlight = "highlight"
+
+const sublinhado_botao = document.getElementById("botao1").addEventListener("click", medo => {estilo_atual= "underline", console.log (numero_erros)})
+const box = document.getElementById("botao2").addEventListener("click", medo => estilo_atual= "box")
+const highlight = document.getElementById("botao3").addEventListener("click", medo => estilo_atual= "highlight")
+
+
+
+const exemplo_sublinhado = annotate(sublinhado, { type: tipo_underline })
+exemplo_sublinhado.show()
+
+const exemplo_quadro = annotate(quadro, { type: tipo_box })
+exemplo_quadro.show()
+
+const exemplo_marcador = annotate(marcador, { type: tipo_highlight, color: "yellow" })
+exemplo_marcador.show()
+
+
+
+
+let numero_erros = 0
+let estilo_atual= "box"
+
+document.getElementById("botao").addEventListener("click", input_txt)
 
 function input_txt() {
-
+    
     let array = texto.value.split(" ")
+    console.log (array.length)
 
     for (let i = 0; i < array.length; i++) {
         let tx = document.createElement('p')
@@ -25,9 +56,18 @@ function input_txt() {
             let erro = document.createElement('h1')
             div_erro.appendChild(erro)
             erro.innerHTML = tx.textContent.replace(",", "")
+            
+        
 
-            console.log(erro)
+          
+            numero_erros++
+            console.log (numero_erros)
 
+            calcular()
+            //console.log(numero_erros)
+
+            
+             
             let desenhar = annotate(tx, {
                 type: estilo_atual,
                 padding: [1, -10, 1, 1],   // [, ]
@@ -35,41 +75,21 @@ function input_txt() {
             })
             desenhar.show()
 
+          
+            
+            
+
+       
+
+    function calcular (){
+    var porcentagem = (numero_erros * 100)/array.length
+    console.log (porcentagem)}
         }
     }
 }
 
-let estilo_atual= "box"
-
-// animacoes do comeco para dar o exemplo bonitinho
-
-const tipo_underline = "underline"
-const tipo_box = "box"
-const tipo_highlight = "highlight"
 
 
-
-const sublinhado_botao = document.getElementById("botao1").addEventListener("click", medo => estilo_atual= "underline")
-const box = document.getElementById("botao2").addEventListener("click", medo => estilo_atual= "box")
-const highlight = document.getElementById("botao3").addEventListener("click", medo => estilo_atual= "highlight")
-
-
-
-
-
-
-const sublinhado = document.getElementsByClassName("sublinhado")[0]
-const quadro = document.getElementsByClassName("quadro")[0]
-const marcador = document.getElementsByClassName("marcador")[0]
-
-const exemplo_sublinhado = annotate(sublinhado, { type: tipo_underline })
-exemplo_sublinhado.show()
-
-const exemplo_quadro = annotate(quadro, { type: tipo_box })
-exemplo_quadro.show()
-
-const exemplo_marcador = annotate(marcador, { type: tipo_highlight, color: "yellow" })
-exemplo_marcador.show()
 
 // ---------------------------------------------------
 
